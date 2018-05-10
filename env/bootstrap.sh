ac5bd59516e986bdf30dff1b2a0f6b8399efa6c0
Holger Woltersdorf <hw@hollo.me>
2018-05-10 14:31:08 +0200

    Add php7.2 vagrant environment

--- env/bootstrap.sh
+++ env/bootstrap.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 apt-get update
+apt-get install php7.2-fpm php7.2-xml -y
+apt-get autoremove --purge -y
+apt-get autoclean
 
 touch /var/run/php-uds.sock && sudo chown www-data:www-data /var/run/php-uds.sock
 
-ln -sf /vagrant/env/php-fpm/7.1/network-socket.pool.conf /etc/php/7.1/fpm/pool.d/network-pool.conf
-ln -sf /vagrant/env/php-fpm/7.1/unix-domain-socket.pool.conf /etc/php/7.1/fpm/pool.d/unix-domain-socket-pool.conf
+ln -sf /vagrant/env/php-fpm/7.2/network-socket.pool.conf /etc/php/7.2/fpm/pool.d/network-pool.conf
+ln -sf /vagrant/env/php-fpm/7.2/unix-domain-socket.pool.conf /etc/php/7.2/fpm/pool.d/unix-domain-socket-pool.conf
 
 cd /vagrant
 
@@ -14,4 +17,4 @@ composer self-update
 chmod -R 0777 /home/vagrant/.composer
 chmod -R 0777 /tmp
 
-service php7.1-fpm restart
+service php7.2-fpm restart
